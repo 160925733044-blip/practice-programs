e575dfbc7cb7c40ceb0cd19babacaec85c93978f
Abdul Wadood <325982936+160925733044-blip@users.noreply.github.com>
2026-09-09 15:03:31 +0530

    Create perfect_number.py

---
+++ perfect_number.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+if num <= 0:
+    print("Not a perfect number")
+else:
+    total = 0
+
+    for i in range(1, num):
+        if num % i == 0:
+            total += i
+
+    if total == num:
+        print("Perfect number")
+    else:
+        print("Not a perfect number")

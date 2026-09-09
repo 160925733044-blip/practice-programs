d7da9ad1059183b91eea9a56648bd1467f08e125
Abdul Wadood <325982936+160925733044-blip@users.noreply.github.com>
2026-09-09 14:20:18 +0530

    Add triangle type classification based on sides

---
+++ triangle_type.py
@@ -0,0 +1,14 @@
+a = float(input("Enter first side: "))
+b = float(input("Enter second side: "))
+c = float(input("Enter third side: "))
+
+if a <= 0 or b <= 0 or c <= 0:
+    print("Invalid triangle")
+elif a + b <= c or a + c <= b or b + c <= a:
+    print("Not a valid triangle")
+elif a == b == c:
+    print("Equilateral triangle")
+elif a == b or b == c or a == c:
+    print("Isosceles triangle")
+else:
+    print("Scalene triangle")

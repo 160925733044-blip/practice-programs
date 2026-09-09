68ed881c73667e4fb184c7afa9ed802e2fcac1b5
Abdul Wadood <325982936+160925733044-blip@users.noreply.github.com>
2026-09-09 14:15:18 +0530

    Create  ticket_pricing.py

---
+++ ticket_pricing.py
@@ -0,0 +1,13 @@
+age = int(input("Enter customer's age: "))
+
+# Example pricing rules
+if age < 5:
+    price = 0
+elif age <= 12:
+    price = 50
+elif age <= 59:
+    price = 100
+else:
+    price = 60
+
+print("Ticket price: ₹", price)
